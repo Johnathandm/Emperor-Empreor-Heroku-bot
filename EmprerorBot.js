30d7f21dbaf4b5d12f6240c00b8483227d2153ca
SmexySnoopy <aljeddah40@gmail.com>
2017-11-17 22:15:10 -0500

    Create EmprerorBot.js

---
+++ EmprerorBot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+const TOKEN = "MzY1OTExMDExMTQ1OTQwOTkz.DLlOKg.p6WWVXn2QohNZJ3Eifb4hS6GLSw";
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    console.log("IM IMMORTAL BITCH!")
+});
+bot.on("message", function(message) {
+   if (message.author.equals(bot.user)) return;
+
+   if (message.content == "Hi!") {
+        message.reply.sendMessage('Hi, Faggot');
+   }
+});
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
